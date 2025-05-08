259159c362fa0d3958ae93d770d365d25e50b628
mdorokhov001 <m.u.dorokhov@mail.ru>
2025-05-08 12:05:25 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ru.main.managementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens ru.main.managementsystem to javafx.fxml;
+    exports ru.main.managementsystem;
+}
